5839e2197841973fa6b095f94e8ec098c1885daa
MartinT86 <martin.tierney86@hotmail.co.uk>
2016-11-14 20:27:00 +0000

    tests setup

---
+++ test/Site.Tests/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            var a = 1;
+            var b = 1;
+            Assert.True(a == b);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var a = 1;
+            var b = 2;
+            Assert.True(a != b);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var a = 5;
+            var b = 5;
+            Assert.True(a == b);
+        }
+    }
+}
