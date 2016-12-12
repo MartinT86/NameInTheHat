709ac7db93019c0db03c68c9197692addf1c36f0
MartinT86 <martin.tierney86@hotmail.co.uk>
2016-12-12 23:22:43 +0000

    less setup

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require("gulp"),
   rimraf = require("rimraf"),
   concat = require("gulp-concat"),
   cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify");
+  uglify = require("gulp-uglify"),
+  less = require("gulp-less");
 
 var paths = {
   webroot: "./src/Site/wwwroot/"
@@ -13,7 +14,9 @@ var paths = {
 
 paths.js = paths.webroot + "js/custom/**/*.js";
 paths.minJs = paths.webroot + "js/custom/**/*.min.js";
+paths.less = paths.webroot + "css/custom/**/*.less";
 paths.css = paths.webroot + "css/custom/**/*.css";
+paths.cssFolder = paths.webroot + "css/custom";
 paths.minCss = paths.webroot + "css/custom/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
@@ -30,6 +33,14 @@ gulp.task("clean:css", function (cb) {
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
+gulp.task('less', function () {
+  return gulp.src(paths.less)
+    .pipe(less({
+      paths: [ ]
+    }))
+    .pipe(gulp.dest(paths.cssFolder));
+});
+
 gulp.task("min:js", function () {
   return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
     .pipe(concat(paths.concatJsDest))
@@ -37,11 +48,21 @@ gulp.task("min:js", function () {
     .pipe(gulp.dest("."));
 });
 
-gulp.task("min:css", function () {
-  return gulp.src([paths.css, "!" + paths.minCss])
+gulp.task("min:css", ["less"], function () {
+  var stream = gulp.src([paths.css, "!" + paths.minCss])
     .pipe(concat(paths.concatCssDest))
     .pipe(cssmin())
     .pipe(gulp.dest("."));
+    return stream;
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+// gulp.task("min:css", function () {
+//   return gulp.src([paths.css, "!" + paths.minCss])
+//     .pipe(concat(paths.concatCssDest))
+//     .pipe(cssmin())
+//     .pipe(gulp.dest("."));
+// });
+
+gulp.task("test", ["min:css"]);
+
+gulp.task("min", ["less", "min:js", "min:css"]);
