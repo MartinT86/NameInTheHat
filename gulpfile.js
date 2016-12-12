930409a631cb4de061265c1171c4226a373be84d
MartinT86 <martin.tierney86@hotmail.co.uk>
2016-12-12 22:29:46 +0000

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+  rimraf = require("rimraf"),
+  concat = require("gulp-concat"),
+  cssmin = require("gulp-cssmin"),
+  uglify = require("gulp-uglify");
+
+var paths = {
+  webroot: "./src/Site/wwwroot/"
+};
+
+paths.js = paths.webroot + "js/custom/**/*.js";
+paths.minJs = paths.webroot + "js/custom/**/*.min.js";
+paths.css = paths.webroot + "css/custom/**/*.css";
+paths.minCss = paths.webroot + "css/custom/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+
+console.log('test', paths.css);
+
+gulp.task("clean:js", function (cb) {
+  rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+  rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:js", function () {
+  return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+    .pipe(concat(paths.concatJsDest))
+    .pipe(uglify())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+  return gulp.src([paths.css, "!" + paths.minCss])
+    .pipe(concat(paths.concatCssDest))
+    .pipe(cssmin())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:js", "min:css"]);
